37edc4302ac4c572f446afce3c7b038d899461f2
Nelson Ramirez <xhyskoax@gmail.com>
2023-11-13 12:04:49 -0300

    se agrega eliminado de productos, con la lógica en el backend de filtrar todos los productos excluyendo el producto que tenga el id eliminado

--- app/app.js
+++ app/app.js
@@ -1,14 +1,17 @@
 const http = require("http");
 const fs = require("fs/promises");
 const { v4: uuidv4 } = require('uuid');
+const url = require("url");
+
 
 const server = http.createServer(async (req, res) => {
+    //permite capturar o procesar argumentos enviados por la url
+    const urlArgumentos = url.parse(req.url, true);
+
 
     if (req.url == "/api/productos" && req.method == "GET") {
         let productos = await fs.readFile(__dirname + "/data/productos.json", "utf8");
         /* productos = JSON.parse(productos); */
-
-        console.log(productos);
 
         res.setHeader("Content-Type", "application/json");
         res.end(productos);
@@ -50,6 +53,25 @@ const server = http.createServer(async (req, res) => {
         })
 
 
+    }else if(req.url.startsWith("/api/productos?id") && req.method == "DELETE"){
+
+        let id = urlArgumentos.query.id;
+
+        console.log("ID PRODUCTO A ELIMINAR: ", id);
+
+        //PRIMERO LEEMOS TODOS LOS PRODUCTOS
+        let dataJson = await fs.readFile(__dirname + "/data/productos.json", "utf8");
+
+        dataJson = JSON.parse(dataJson);
+
+        //FILTRAMOS LOS PRODUCTOS EXCLUYENDO EL QUE COINCIDE CON EL ID ENVIADO
+        dataJson.productos = dataJson.productos.filter(producto => producto.id != id);
+
+        //VOLVEMOS A GUARDAR LA DATA ACTUALIZADA EN productos.json
+        await fs.writeFile(__dirname + "/data/productos.json", JSON.stringify(dataJson, null, 4), "utf8");
+
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({code: 200, message:"Producto eliminado."}))
     }
     else if (req.url == "/" && req.method == "GET") {
 
